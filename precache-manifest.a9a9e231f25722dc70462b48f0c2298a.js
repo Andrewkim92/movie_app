406ff5b2953c8c27e09c694da627e83046f7ec18
Kim youngwoo <38499381+Andrewkim92@users.noreply.github.com>
2019-01-29 23:40:53 +0900

    Updates

---
+++ precache-manifest.a9a9e231f25722dc70462b48f0c2298a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "80c207a83074c081004b",
+    "url": "/movie_app/static/js/main.80c207a8.chunk.js"
+  },
+  {
+    "revision": "c38eb24b2b2b0ea15eb9",
+    "url": "/movie_app/static/js/1.c38eb24b.chunk.js"
+  },
+  {
+    "revision": "80c207a83074c081004b",
+    "url": "/movie_app/static/css/main.b05951fc.chunk.css"
+  },
+  {
+    "revision": "5fc84dd772f2ff3e766d71c150f7c417",
+    "url": "/movie_app/index.html"
+  }
+];
